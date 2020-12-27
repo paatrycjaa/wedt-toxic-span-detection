7481a20951962c195cf1f6cfd7449c540a0ea9dd
Julia Klos <julia.klos.stud@pw.edu.pl>
2020-12-27 23:49:13 +0100

    detecting words by attention-no spans

--- src/DataProcessing.py
+++ src/DataProcessing.py
@@ -5,11 +5,11 @@ class DataProcessing:
     def extract_toxity(self):
         return
     def get_classes_amount(self, train_df):
-        seriesObj = train_df.apply(lambda x: True if x.toxicity_sentence[0]==1.0 else False, axis=1)
+        seriesObj = train_df.apply(lambda x: True if x.toxicity==1.0 else False, axis=1)
         numOfRows1 = len(seriesObj[seriesObj == True].index)
         numOfRows0 = len(seriesObj[seriesObj == False].index)
         return (numOfRows1, numOfRows0)
     def get_missing_class_elements(self,df, N, classValue):
-        # mask = df['toxicity_sentence'] == classValue
-        # new_df = df[mask]
-        return df.sample(N)
+        mask = df['toxicity'] == classValue
+        new_df = df[mask]
+        return new_df.sample(N)
